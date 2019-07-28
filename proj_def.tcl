4331841770a01dd4281898965ac9d064e0bfc160
Wojciech M. Zabołotny <wzab01@gmail.com>
2019-07-28 23:30:06 +0200

    Project updated to Vivado 2017.4

--- proj_def.tcl
+++ proj_def.tcl
@@ -2,12 +2,16 @@ set eprj_proj_name "TE0712_i2c_Forth_controller"
 set eprj_def_root "src"
 set eprj_top_entity "i2c_j1b_ctrl_top"
 set eprj_board_part ""
+set eprj_board_files_path ""
 set eprj_part "xc7a100tfgg484-1"
 set eprj_target_language "VHDL"
 set eprj_simulator_language "Mixed"
 set eprj_default_lib "work"
-set eprj_synth_flow "Vivado Synthesis 2015"
+set eprj_synth_flow "Vivado Synthesis 2017"
 set eprj_synth_strategy "Flow_PerfOptimized_high"
-set eprj_impl_flow "Vivado Implementation 2015"
+set eprj_impl_flow "Vivado Implementation 2017"
 set eprj_impl_strategy "Performance_Retiming"
+set eprj_vivado_version "2017.4"
+set eprj_vivado_version_allow_upgrade false
+
 
