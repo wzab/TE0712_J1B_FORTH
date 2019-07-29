5de7f78641be08941eef132a2e69cc85fa4690e7
Wojciech M. Zabołotny <wzab01@gmail.com>
2019-07-29 13:04:40 +0200

    Modifications done by Michał Kruszewski to make shell compatible with Python3. My modififcations to enable automatic building of Forth firmware.

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
+(
+  cd original/j1b/verilator
+  ./localtest_te0712_gen_prog
+)
+(
+  cd src/j1b
+  ./ram_init.py mem_dump.hex
+)
 vivado -mode batch -source eprj_create.tcl
 vivado -mode batch -source eprj_build.tcl
 
